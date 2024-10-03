561970830ff88f4e4670edfcbc8fd9ffbd29a71d
Jean-luc G <jean-luc.gouthon@imsp-uac.org>
2024-10-03 04:36:12 +0200

    gt_a 1

---
+++ GNN/README.md
@@ -0,0 +1,3 @@
+# Le modèle GT-A 
+
+## Ce tuto décrit le modèle GT-A avec une matrice d'adjence construite à partir de la matrice de dégrés au lieu de la matrice angulaire
--- GNN/gt_a_model.py
+++ GNN/gt_a_model.py
@@ -135,7 +135,7 @@ class GCNMultiLayer(nn.Module):
     
 
 class EncoderLayer(nn.Module):
-    def __init__(self, n_features=100, head=5, feed_forward_dim = 64, hid_linear_part_dim = 64, n_predictions = 10):
+    def __init__(self, n_features=100, head=5, feed_forward_dim = 64):
         # d_model représente le nombre de features dans X et dans notre cas c'est la taille de la dimension de l'âge
         super(EncoderLayer,self).__init__()
         # Ici on suppose que Q, K, V in R^(T x d_model)
@@ -143,24 +143,31 @@ class EncoderLayer(nn.Module):
         self.d_K = n_features
         self.d_V = n_features
         self.head = head
-        self.n_predictions = n_predictions
-        self.weight_Q = nn.Parameter(torch.FloatTensor(n_features, self.d_K))
-        self.weight_K = nn.Parameter(torch.FloatTensor(n_features, self.d_K))
-        self.weight_V = nn.Parameter(torch.FloatTensor(n_features, self.d_V))
-        self.weight_multi = nn.Parameter(torch.FloatTensor(self.head * n_features, n_features))
+        
+        #Définition des poids
+        self.weight_Q = nn.ParameterList([nn.Parameter(torch.FloatTensor(n_features, self.d_K)) for _ in range(head)])
+        self.weight_K = nn.ParameterList([nn.Parameter(torch.FloatTensor(n_features, self.d_K)) for _ in range(head)])
+        self.weight_V = nn.ParameterList([nn.Parameter(torch.FloatTensor(n_features, self.d_V)) for _ in range(head)])
+        self.weight_multi = nn.Parameter(torch.FloatTensor(n_features * head, n_features))
 
         self.ffn = nn.Sequential(
             nn.Linear(n_features, feed_forward_dim),
             nn.ReLU(),
             nn.Linear(feed_forward_dim, n_features)
         )
-        self.layer_norm1 = None
-        self.layer_norm2 = None
+        self.layer_norm1 = nn.LayerNorm(n_features)
+        self.layer_norm2 = nn.LayerNorm(n_features)
+    
+        # Initialisation des poids d'attention
+        self.initiate_parameters()
     
     def initiate_parameters(self):
-        nn.init.xavier_normal_(self.weight_K)
-        nn.init.xavier_normal_(self.weight_Q)
-        nn.init.xavier_normal_(self.weight_V)
+        # Initialisation avec Xavier pour chaque tête
+        for i in range(self.head):
+            nn.init.xavier_normal_(self.weight_K[i])
+            nn.init.xavier_normal_(self.weight_Q[i])
+            nn.init.xavier_normal_(self.weight_V[i])
+        nn.init.xavier_normal_(self.weight_multi)
 
 
     def forward(self,X):
@@ -170,46 +177,30 @@ class EncoderLayer(nn.Module):
 
         """
         self.n_times = X.shape[0]
-
-        if self.layer_norm1 is None:
-            self.layer_norm1 = nn.LayerNorm(X.shape)
-        if self.layer_norm2 is None:
-            self.layer_norm2 = nn.LayerNorm(X.shape)
-
         
-        # initialisation des paramètres d'attention pour la head 0
-        self.initiate_parameters()
-
-        Q_0 = torch.mm(X, self.weight_Q)
-        K_0 = torch.mm(X, self.weight_K)
-        V_0 = torch.mm(X, self.weight_V)
-        Attention_0 = torch.mm(F.softmax(torch.mm(Q_0, K_0.T)/((self.d_K)**(0.5))), V_0)
 
         # enregistrement de la première attention calculée
-        multi_head_Attention = Attention_0
+        multi_head_Attention = []
 
         #####multi_head_Attention = torch.FloatTensor(X.shape)
-        for i in range(1,self.head):
-            self.initiate_parameters()
-
-            Q_i = torch.mm(X, self.weight_Q)
-            K_i = torch.mm(X, self.weight_K)
-            V_i = torch.mm(X, self.weight_V)
-            Attention_i = torch.mm(F.softmax(torch.mm(Q_i, K_i.T)/((self.d_K)**(0.5))), V_i)
-            multi_head_Attention = torch.concat((multi_head_Attention, Attention_i),dim=1)
+        for i in range(self.head):
+            Q_i = torch.mm(X, self.weight_Q[i])
+            K_i = torch.mm(X, self.weight_K[i])
+            V_i = torch.mm(X, self.weight_V[i])
+            Attention_i = torch.mm(F.softmax(torch.mm(Q_i, K_i.T)/(self.d_K ** 0.5)), V_i)
+            multi_head_Attention.append(Attention_i)
         
+        multi_head_Attention = torch.concat(multi_head_Attention, dim=1)
 
-        # initialisation de la matrice de poids du multi-head attention
-        nn.init.xavier_normal_(self.weight_multi)
 
         # multi-head = concat(head_0,...,head_h) W^(0) où W^(0) in R^(head*n_features, n_features)
-        multi_head_Attention = torch.mm(multi_head_Attention, self.weight_multi)
+        result = torch.matmul(multi_head_Attention, self.weight_multi)
 
         # X = LayerNorm(X + self_Multi_head(X))
         # La normalisation pour chaque exemple x(pour chaque t dans notre cas) est faite sur la dimension des features, d'où la 
         # présence de d_model en argument. Pour être plus clair, on considère la moyenne et l'écart-type sur toutes les
         # features pour chaque exemple pris: ^x = (x-u)/s où u est la moyenne sur toutes les features et s l'écart-type
-        X = X + multi_head_Attention
+        X = X + result
         X = self.layer_norm1(X)
         ffn_x = self.ffn(X)
         X = X + ffn_x
@@ -223,9 +214,9 @@ class Encoder(nn.Module):
         self.n_predictions = n_predictions
         self.n_features = n_features
         self.d_model = n_features
-        self.encod1 = EncoderLayer(n_features)
-        self.encod2 = EncoderLayer(n_features)
-        self.encod3 = EncoderLayer(n_features)
+        self.encod1 = EncoderLayer(n_features=self.n_features)
+        self.encod2 = EncoderLayer(n_features=self.n_features)
+        self.encod3 = EncoderLayer(n_features=self.n_features)
 
         self.linear_part = nn.Sequential(
             nn.Linear(self.n_features, hid_linear_part_dim),
@@ -246,45 +237,48 @@ class Encoder(nn.Module):
             self.n_countries = X.shape[0]
         if self.n_times is None:
             self.n_times = X.shape[1]
-        #self.n_features = X.shape[2]
+        
 
         # Initialisation de la matrice de prévisions
-        #y = torch.FloatTensor(self.n_countries, self.n_predictions, self.n_features)
-        y = torch.FloatTensor(self.n_countries, self.n_times, self.n_features)
+        y = torch.FloatTensor(self.n_countries, self.n_predictions, self.n_features)
+        #y = torch.zeros(self.n_countries, self.n_times, self.n_features)
 
 
         # Tel que l'encoder est défini c'est à appliquer pour chaque pays. Donc en partant de la matrice X, sortie du réseau
         # de neurone graphique, X.shape = (n_countries, n_times, n_ages). Donc on applique l'encoder pour chaque pays de X 
         # pour évaluer uniquement la dépendance spatiale.
         for country in range(self.n_countries):
-            x = self.encod1.forward(X[country]) # x.shape = [n_times, n_features]
+            x = self.encod1(X[country]) # x.shape = [n_times, n_features]
             x = self.encod2(x)
             x = self.encod3(x)
 
             
             # La partie linéaire pour faire les prévisions pour chaque pays. La prévision étant faite jusqu'à l'horizon 
             # de 10 années.
-            #current_input = x
-            #predictions = []
-            #for _ in range(self.n_predictions):
-            #    next_pred = self.linear_part(current_input[-1])
-            #    predictions.append(next_pred.squeeze(0)) # enregistrer la prediction
+            current_input = x
+            predictions = []
+            for _ in range(self.n_predictions):
+                next_pred = self.linear_part(current_input[-1])
+                predictions.append(next_pred.squeeze(0)) # enregistrer la prediction
 
-            #    current_input = torch.cat([current_input, next_pred.unsqueeze(0)],dim=0)
+                current_input = torch.cat([current_input, next_pred.unsqueeze(0)],dim=0)
 
-            #y[country] = torch.stack(predictions, dim=0)
+            y[country] = torch.stack(predictions, dim=0)
 
-            y[country] = x
+            #y[country] = x.clone()
         
         return y
 
 
 
+
+
+
 class GTA_Model(nn.Module):
-    def __init__(self, in_features=100, hid_features=60, out_features=100):
+    def __init__(self, in_features=100, hid_features=60,n_predictions=10, out_features=100):
         super(GTA_Model, self).__init__()
         self.gcn = GCNMultiLayer(in_features, hid_features, out_features)
-        self.encoder = Encoder(n_predictions=9)
+        self.encoder = Encoder(n_predictions=n_predictions)
     
     def forward(self, X, A):
         X = self.gcn(X ,A)
